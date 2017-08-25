5b98ccfa65bc26dcacfa9dd1225c9f013b1ca10b
Byron Allen <byron.a.allen@gmail.com>
2017-08-25 23:20:08 +1000

    adding column_name

--- run.py
+++ run.py
@@ -12,16 +12,17 @@ import pandas as pd
 directory='./' + raw_input('What directory would you like to read from?: ')
 output_directory='./' + raw_input('What directory would you like to write to?: ')
 output_file='/' + raw_input('Save output file as?:')
+column_name=raw_input('What is the name of the column you want to use?: ')
 csvs=[csv for csv in os.listdir(directory) if csv.endswith('.csv') == True]
 length_of_dataset=pd.read_csv(directory + '/' + csvs[0]).shape[0]
 df=pd.DataFrame(index=range(length_of_dataset))
 
-# Iterate through designated folder and combine the 'y' field in each CSV
+# Iterate through designated folder and combine the column_name field in each CSV
 for csv in csvs:
     file_path = directory + '/' + csv
-    new_df=pd.read_csv(file_path, index_col=0)[['y']]
-    new_df[csv.split('.')[0]]=new_df.y
-    new_df.drop(labels='y', inplace=True, axis=1)
+    new_df=pd.read_csv(file_path, index_col=0)[[column_name]]
+    new_df[csv.split('.')[0]]=new_df[column_name]
+    new_df.drop(labels=column_name, inplace=True, axis=1)
     df=df.merge(new_df, left_index=True, right_index=True)
 
 # Save 'combineX' table as csv
